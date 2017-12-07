8cf0a30845844b4abb7157635f677f49849befb4
ccbrantley <ccbrantley@gtcc.edu>
2017-12-07 14:15:59 -0500

    Update InchWorm.h

--- Classes/InchWorm/InchWorm.h
+++ Classes/InchWorm/InchWorm.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include "Windows.h"
+using namespace std;
+class InchWorm
+{
+public:
+	InchWorm();
+	string InchWorm::firstMove(int);
+	string InchWorm::secondMove(int);
+	string InchWorm::thirdMove(int);
+	void InchWorm::continualMove(int);
+	~InchWorm();
+};
 
