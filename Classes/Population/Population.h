69965de8f9ad2b5c86f291fc5fc84459887904bd
ccbrantley <ccbrantley@gtcc.edu>
2017-12-12 19:01:06 -0500

    Update Population.h

--- Classes/Population/Population.h
+++ Classes/Population/Population.h
@@ -1,19 +1,23 @@
 #pragma once
+#include <sstream>
+#include <string>
+#include <iomanip>
+using namespace std;
 class Population
 {
 public:
 	Population();
-	Population(double population, int birth, int death);
+	Population(double population, double birth, double death);
 	~Population();
 	void Population::setPopulation(double);
-	void Population::setBirth(int);
-	void Population::setDeath(int);
+	void Population::setBirth(double);
+	void Population::setDeath(double);
 	double Population::getPopulation();
-	int Population::getBirth();
-	int Population::getDeath();
+	double Population::getBirth();
+	double Population::getDeath();
 	double Population::getBirthRate();
 	double Population::getDeathRate();
+	string Population::print();
 private:
-	double population;
-	int birth, death, birthRate, deathRate;
+	double population, birthRate, deathRate, birth, death;
 };
